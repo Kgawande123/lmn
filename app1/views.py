101f056dbe78f20d9aca15485d66fd15a79c26e8
kalyani <kalyani.gawande@rssc.edu.in>
2024-06-24 13:19:26 +0530

    add new line

--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+print("This is my 1st oage")
 # Create your views here.
 def feature(request):
     return HttpResponse("ADD")
